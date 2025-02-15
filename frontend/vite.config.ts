50cf55fd8be61adef4a95430a5371e32cea36150
Jon Manning <jon@secretlab.com.au>
2025-02-16 10:03:21 +1100

    Add compression step to frontend build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from "vite-plugin-svgr"
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,8 @@ export default defineConfig({
     react(),
     tailwindcss(),
     svgr(),
+    viteCompression({
+      algorithm: "brotliCompress"
+    }),
   ],
 })
