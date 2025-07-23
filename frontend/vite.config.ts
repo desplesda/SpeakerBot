c0c24eaf73d9990954090570d1e22120e845a294
Jon Manning <jon@secretlab.com.au>
2025-07-23 17:57:29 +1000

    Add vite-bundle-analyzer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from "vite-plugin-svgr"
 import viteCompression from 'vite-plugin-compression';
+import { analyzer as bundleAnalyzer } from 'vite-bundle-analyzer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
     svgr(),
     viteCompression({
       algorithm: "brotliCompress"
+    }),
+    bundleAnalyzer({
+      analyzerMode: "static",
+      openAnalyzer: false,
+      summary: false,
     }),
   ],
 })
